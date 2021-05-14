cc28165ccf1970629560c5802f017f7176475a8a
lekanda <abernaolao@gmail.com>
2021-05-14 15:08:12 +0200

    410.Creando URLs de las paginas que no es necesario autenticarse.

---
+++ controllers/PaginasController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Controllers;
+
+
+class PaginasController {
+
+    public static function index () {
+        echo 'Desde Index';
+    }
+
+    public static function nosotros () {
+        echo 'Desde Nosotros';
+    }
+
+    public static function propiedades () {
+        echo 'Desde propiedades';
+    }
+
+    public static function propiedad () {
+        echo 'Desde propiedad';
+    }
+
+    public static function blog () {
+        echo 'Desde blog';
+    }
+
+    public static function entrada () {
+        echo 'Desde entrada';
+    }
+
+    public static function contacto () {
+        echo 'Desde contacto';
+    }
+    
+}
--- public/index.php
+++ public/index.php
@@ -5,21 +5,22 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 use Controllers\PropiedadController;
 use Controllers\VendedorController;
+use Controllers\PaginasController;
 
 $router = new  Router();
-
 // debuguear(PropiedadController::class);
 
-// Rutas propiedades
+/**** Zona Privada ****/
+// Rutas Propiedades
 $router->get('/admin', [PropiedadController::class, 'index']);
 $router->get('/propiedades/crear', [PropiedadController::class, 'crear']);
 $router->post('/propiedades/crear', [PropiedadController::class, 'crear']);
 $router->get('/propiedades/actualizar', [PropiedadController::class, 'actualizar']);
 $router->post('/propiedades/actualizar', [PropiedadController::class, 'actualizar']);
 $router->post('/propiedades/eliminar', [PropiedadController::class, 'eliminar']);
+// ::class => Para que tome el namespace
 
-
-// Rutas vendedores
+// Rutas Vendedores
 $router->get('/vendedores/crear', [VendedorController::class, 'crear']);
 $router->post('/vendedores/crear', [VendedorController::class, 'crear']);
 $router->get('/vendedores/actualizar', [VendedorController::class, 'actualizar']);
@@ -27,6 +28,20 @@ $router->post('/vendedores/actualizar', [VendedorController::class, 'actualizar'
 $router->post('/vendedores/eliminar', [VendedorController::class, 'eliminar']);
 
 
+
+/**** Zona Publica ****/
+// Paginas libres de Autenticacion.
+$router->get('/', [PaginasController::class, 'index']);
+$router->get('/nosotros', [PaginasController::class, 'nosotros']);
+$router->get('/propiedades', [PaginasController::class, 'propiedades']);
+$router->get('/propiedad', [PaginasController::class, 'propiedad']);
+$router->get('/blog', [PaginasController::class, 'blog']);
+$router->get('/entrada', [PaginasController::class, 'entrada']);
+$router->get('/contacto', [PaginasController::class, 'contacto']);
+$router->post('/contacto', [PaginasController::class, 'contacto']);
+
+
+
+
+
 $router->comprobarRutas();
-
-
