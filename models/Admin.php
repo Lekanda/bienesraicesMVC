6462a51463fbf0d86f08c5ec117d1d96dc374239
lekanda <abernaolao@gmail.com>
2021-05-18 17:21:03 +0200

    424.Crear el modelo de Autenticacion.

--- models/Admin.php
+++ models/Admin.php
@@ -3,5 +3,20 @@
 namespace Model;
 
 class Admin extends ActiveRecord{
+    // Base de Datos. Asocia la tabla USUARIOS con el modelo Admin.php
+    protected static $tabla = 'usuarios';
+    protected static $columnasDB = ['id','email','password'];
+
+    public $id;
+    public $email;
+    public $password;
+
+    // Constructor
+    public function __construct($args = []){
+        $this->id = $args['id'] ?? null;
+        $this->email = $args['email'] ?? '';
+        $this->password = $args['password'] ?? '';
+    }
+
     
 }
