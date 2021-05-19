8c465fd1132d3443b00c75e9358c0dee8d6aff49
lekanda <abernaolao@gmail.com>
2021-05-19 23:11:03 +0200

    434.Comprobando que un elemento exista en Cipress.

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -1,12 +1,13 @@
 /// <references types="cypress"/>
 // Da mejor autocompletado
 describe('Carga la pagina principal', () =>{
-    it('Prueba el header de la pagina principal', () => {
-        cy.visit('/public/index.php');
-        cy.get('[data-cy="heading-sitio"]');
-        cy.url()
-        cy.location('href')
+    it('Prueba el Header de INDEX', () => {
+        cy.visit('/');
+        cy.get('[data-cy="heading-sitio"]').should('exist');// Comprueba sí existe el atributo
+        cy.get('[data-cy="heading-sitio"]').invoke('text').should('equal', 'Venta de Casas De Lujo y Apartamentos Exclusivos');// Comprueba que sea texto y que el textop sea igual a 'Venta de......'
+        cy.get('[data-cy="heading-sitio"]').invoke('text').should('not.equal', 'Bienes Raices');// Comprueba que sea texto y que el texto NO  sea igual a 'Bienes Raices'
     });
+    
 });
 
 /** 
