945a21b8c6dcd5c99a3ec6fd7cbe76e712afedba
lekanda <abernaolao@gmail.com>
2021-05-21 11:01:20 +0200

    441.Probando el formulario de contacto con Cypress.

--- views/paginas/contacto.php
+++ views/paginas/contacto.php
@@ -11,7 +11,7 @@
         <img loading="lazy" src="build/img/destacada3.jpg" alt="Imagen Contacto">
     </picture>
 
-    <h2>Llene el formulario de Contacto</h2>
+    <h2 data-cy="heading-formulario">Llene el formulario de Contacto</h2>
 
     <form class="formulario" action="/contacto" method="POST">
         <fieldset>
